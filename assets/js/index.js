6fcf34e5a008a306fc3a43d823528b13a45e4598
workwithpassion <138347013+workwithpassion@users.noreply.github.com>
2025-11-14 22:57:52 +0700

    add image

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ import { newsData } from "../data/newsData.js";
 // 1. Render Featured News
 function renderFeatured() {
   const { title, description, image } = newsData.featured;
-  document.querySelector('.boxupperleft__img img').src = `/client/assets/${image}`;
+  document.querySelector('.boxupperleft__img img').src = `../assets/${image}`;
   document.querySelector('.boxupperleft__description h1').textContent = title;
   document.querySelector('.boxupperleft__description p').textContent = description;
 }
@@ -25,7 +25,7 @@ function renderThreeNews() {
   container.innerHTML = newsData.threeNews.map(item => `
     <div class="threenews__oii">
       <div class="threenews__img">
-        <img src="/client/assets/${item.image}" alt="">
+        <img src="../assets/${item.image}" alt="">
       </div>
       <div class="threenews__description">
         <h2>${item.title}</h2>
@@ -40,13 +40,13 @@ function renderLifestyle() {
   const { big, small } = newsData.lifestyle;
 
   // Big news
-  document.querySelector('.bignews__img img').src = `/client/assets/${big.image}`;
+  document.querySelector('.bignews__img img').src = `../assets/${big.image}`;
   document.querySelector('.bignews__description p').textContent = big.title;
 
   // Small news
   const containers = document.querySelectorAll('.smallnews > div');
   small.forEach((item, i) => {
-    containers[i].querySelector('.smallnews__img img').src = `/client/assets/${item.image}`;
+    containers[i].querySelector('.smallnews__img img').src = `../assets/${item.image}`;
     containers[i].querySelector('.smallnews__description p').textContent = item.title;
   });
 }
@@ -63,7 +63,7 @@ function renderTopics() {
     const newsHTML = topic.news.map(item => `
       <div class="news">
         <div class="news__img">
-          <img src="/client/assets/${item.image}" alt="">
+          <img src="../assets/${item.image}" alt="">
         </div>
         <div class="news__description">
           <p>${item.title}</p>
@@ -81,7 +81,7 @@ function renderAI() {
   const { main, sideNews } = newsData.ai;
 
   // Main AI news
-  document.querySelector('.AI_maintopic .AI__img img').src = `/client/assets/${main.image}`;
+  document.querySelector('.AI_maintopic .AI__img img').src = `../assets/${main.image}`;
   document.querySelector('.AI_maintopic .AI__description h3').textContent = main.title;
   document.querySelector('.AI_maintopic .AI__description p').textContent = main.description;
 
@@ -89,7 +89,7 @@ function renderAI() {
   const newsContainers = document.querySelectorAll('.AI__right > div:not(.block)');
   sideNews.forEach((item, i) => {
     if (newsContainers[i]) {
-      newsContainers[i].querySelector('.img__container img').src = `/client/assets/${item.image}`;
+      newsContainers[i].querySelector('.img__container img').src = `../assets/${item.image}`;
       newsContainers[i].querySelector('.AI__title p').textContent = item.title;
     }
   });
@@ -102,13 +102,13 @@ function renderPopular() {
   // Grid 1 - Most Popular
   const grid1Contents = document.querySelectorAll('.grid__box1 > div:not(.grid1__title)');
   grid1.forEach((item, i) => {
-    grid1Contents[i].querySelector('img').src = `/client/assets/${item.image}`;
+    grid1Contents[i].querySelector('img').src = `../assets/${item.image}`;
     grid1Contents[i].querySelector('h2').textContent = item.category;
     grid1Contents[i].querySelector('p').textContent = item.title;
   });
 
   // Grid 2 - Business Main
-  document.querySelector('.business__news-img img').src = `/client/assets/${businessMain.image}`;
+  document.querySelector('.business__news-img img').src = `../assets/${businessMain.image}`;
   document.querySelector('.business__title').textContent = businessMain.category;
   document.querySelector('.p2').textContent = businessMain.title;
   document.querySelector('.business__news-description p').textContent = businessMain.description;
@@ -116,7 +116,7 @@ function renderPopular() {
   // Grid 3
   const grid3Containers = [document.querySelector('.lifestyle'), document.querySelector('.politics')];
   grid3.forEach((item, i) => {
-    grid3Containers[i].querySelector('img').src = `/client/assets/${item.image}`;
+    grid3Containers[i].querySelector('img').src = `../assets/${item.image}`;
     grid3Containers[i].querySelector('h2').textContent = item.category;
     grid3Containers[i].querySelector('p').textContent = item.title;
   });
@@ -204,7 +204,7 @@ function setupClickHandlers() {
   const aiMain = document.querySelector('.AI_maintopic');
   if (aiMain && newsData.ai.main.id) {
     aiMain.addEventListener('click', () => {
-      window.location.href = `/pages/Post.html?id=${newsData.ai.main.id}`;
+      window.location.href = `./Post.html?id=${newsData.ai.main.id}`;
     });
   }
 
