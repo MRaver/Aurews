c2dcfedb6ee4b1ea5520860b51c0e12ce002bf3a
workwithpassion <138347013+workwithpassion@users.noreply.github.com>
2025-11-17 20:25:37 +0700

    jiji

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,14 +2,14 @@
 function openPopup(tab) {
   document.getElementById("popupOverlay").classList.add("active");
   document.getElementById("mainContent").classList.add("blur");
-//   document.getElementById("mobile-menu__placeholder").classList.add("blur");
+  //   document.getElementById("mobile-menu__placeholder").classList.add("blur");
   switchTab(tab);
 }
 
 function closePopup() {
   document.getElementById("popupOverlay").classList.remove("active");
   document.getElementById("mainContent").classList.remove("blur");
-//   document.getElementById("mobile-menu__placeholder").classList.remove("blur");
+  //   document.getElementById("mobile-menu__placeholder").classList.remove("blur");
 }
 
 function closePopupOnOverlay(event) {
